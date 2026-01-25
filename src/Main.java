4ca0dd550c46b89ec6cb7711c8fd7610cfb78671
therobinplay <therealrobinplays@gmail.com>
2026-01-25 07:54:29 +0545

    update

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,65 @@
-
 import controller.OrdersController;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
     static HashMap<String, String> users = new HashMap<>();
+    static final String FILE_NAME = "user.txt";
 
     public static void main(String[] args) {
+        loadUsers();
+
         Scanner scan = new Scanner(System.in);
 
-        System.out.println(" Welcome to Robin Dai ko Cafe !");
+        System.out.println("Welcome to Robin Dai ko Cafe!");
 
         System.out.print("Enter your phone number: ");
         String phone = scan.nextLine();
 
         if (users.containsKey(phone)) {
-            System.out.println("Welcome back, " + users.get(phone) + " !");
+            System.out.println("Welcome back, " + users.get(phone) + "!");
         } else {
             System.out.print("Enter your name: ");
             String name = scan.nextLine();
             users.put(phone, name);
-            System.out.println("Welcome, " + name + " !");
+            saveUser(phone, name);
+            System.out.println("Welcome, " + name + "!");
         }
 
         OrdersController.main(args);
-
         scan.close();
+    }
+
+    static void loadUsers() {
+        try {
+            File file = new File(FILE_NAME);
+            if (!file.exists()) return;
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length == 2) {
+                    users.put(data[0], data[1]);
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error loading users.");
+        }
+    }
+
+    static void saveUser(String phone, String name) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_NAME, true));
+            bw.write(phone + "," + name);
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Error saving user.");
+        }
     }
 }
--- src/model/OrdersModel.java
+++ src/model/OrdersModel.java
@@ -1,5 +1,8 @@
 package model;
 
 public class OrdersModel {
+    public static void main(String[] args) {
+        
+    }
     
 }
